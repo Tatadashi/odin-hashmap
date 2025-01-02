aaa4b6652d59244b3bd4503b1b17570faf8b4af9
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-01-01 20:53:12 -0800

    Add constructor for bucket/capacity amount

--- main.js
+++ main.js
@@ -3,18 +3,32 @@ import { LinkedList } from "./linkedList.js";
 class HashMap {
     #loadFactor = 0.75;
     #capacity = 16;
+    _buckets;
+
+    constructor (buckets = this.#capacity) {
+        this.#capacity = buckets;
+        this._buckets = new Array(this.#capacity);
+    }
+
+    get buckets() {
+        return this._buckets;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+          hashCode %= 16;
+        }
+
+        return hashCode;
+    }
+    set(key, value) {
+
+    }
 }
 
-function hash(key) {
-let hashCode = 0;
-    
-const primeNumber = 31;
-for (let i = 0; i < key.length; i++) {
-    hashCode = primeNumber * hashCode + key.charCodeAt(i);
-    hashCode %= 16;
-}
-
-return hashCode;
-} 
-
-console.log(hash("dasfajfdsjfenfasdfn"));
+const hor = new HashMap();
+console.log(hor.buckets);
